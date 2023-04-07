94f1999372b9a28f294daf70ea935a5745b4cbab
steroyle <steroyle@gmail.com>
2023-04-07 21:25:51 +0100

    Improved error page with detailed error messages

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,32 @@
+import { Link } from "react-router-dom";
+import { useRouteError } from "react-router-dom"
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
+import { useNavigate } from "react-router-dom";
+
 const Error = () => {
+  const error = useRouteError();
+  const navigation = useNavigate();
   return (
-    <div>Error</div>
+    <div className="error">
+      <h1>Uh oh! We've got a problem</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md">
+        <button
+          className="btn btn--dark"
+          onClick={() => {navigation(-1)}}
+        >
+          <ArrowUturnLeftIcon width={20} />
+          <span>Go Back</span>
+        </button>
+        <Link
+          to="/"
+          className="btn btn--dark"
+        >
+          <HomeIcon width={20} />
+          <span>Go home</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
